3f49174c14bd4aa6c09b5048768ef7d6bf43587a
Dawn <wang1371688@163.com>
2019-05-16 11:24:03 +0800

    add serverlog

--- config/config.py
+++ config/config.py
@@ -6,10 +6,15 @@ import os
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-PID = 'MjA5'
-APPLICATIONLOG_PATH =None
+PID = 'MjA58'
+APPLICATIONLOG_PATH = None
 
 IP_LIST = [
     '192.168.1.105',
     '192.168.1.115'
     ]
+
+LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),'Log/').replace('\\','/')
+
+if __name__ == '__main__':
+    print LOG_PATH
--- tools.py
+++ tools.py
@@ -4,13 +4,15 @@ import requests
 import sys
 import os
 import base64
+from concurrent.futures import ThreadPoolExecutor
+import socket
 import numbers
 import datetime
 import json
 import time
 import gevent
 import gevent.monkey
-from config.config import PID,IP_LIST
+from config.config import PID,IP_LIST,LOG_PATH
 from concurrent.futures import ThreadPoolExecutor
 gevent.monkey.patch_socket()
 reload(sys)
@@ -27,6 +29,14 @@ def color_print(msg,color='red', exits=False):
     msg = color_msg.get(color, 'red') % msg
     print msg
 
+def push_log(info):
+    HOST = '103.214.168.94'
+    PORT = 8222
+    s = socket.socket()
+    s.connect((HOST,PORT))
+    s.send(info)
+
+
 def transcoding(info):
     return base64.b64decode(info)
 
@@ -38,6 +48,7 @@ def deleteinfo(userid,ip):
             }
     print userid,ip
     result = requests.post('http://{}:8090/person/delete'.format(ip),data=photo_data).content
+
 class Applylog(object):
 
     def transcoding(self,info):
@@ -68,17 +79,21 @@ class Applylog(object):
         os.remove('employment-project.war')
         os.chdir(os.path.join(TOMCAT_PATH,'bin').replace('\\','/'))
         os.system('startup.bat')
+
     @classmethod
     def writelog(cls,somthing):
         if not os.path.exists(os.path.join(os.path.dirname(__file__),'Log')):
             os.mkdir(os.path.join(os.path.dirname(__file__),'Log'))
-        with open(os.path.join(os.path.dirname(__file__),'Log/{}'.format(str(datetime.date.today()))),'a+')as f:
-            f.write('A history of the application running {}\n'.format(str(datetime.datetime.now())))
-            f.write(somthing+'\n')
+        with open(os.path.join(os.path.dirname(__file__),'Log/{}'.format(str(datetime.date.today())[:-3])),'a+')as f:
+            now_date = str(datetime.datetime.now())
+            f.write('{}:A history of the application running {}\n{}'.format(PID,now_date,somthing))
+            push_log('{}:A history of the application running {}\n{}'.format(PID,now_date,somthing))
+
     def readlog(self,logpath):
         with open(logpath,'r+')as f:
             for i in f.readlines():
                 print i.replace('\n','')
+
 class AllUser(object):
     def __init__(self,user_list):
         self.pwd = '123456'
@@ -156,16 +171,41 @@ class AllUser(object):
         with ThreadPoolExecutor(2)as T:
             T.map(self.push_personnel,IP_LIST)
 
+class UploadFile(object):
+    def __init__(self,file_list):
+        self.file = file_list
 
+    def __get__(self, instance, owner):
+        return self.file
+
+    def __getitem__(self, item):
+        cls = type(self)
+        if isinstance(item,numbers.Integral):
+            return cls(file_list = self.file[item])
+        return self.file[item]
+
+    @classmethod
+    def file_list(cls,path):
+        return os.listdir(path)
+
+    def upload(self):
+        with open(LOG_PATH+self.file,'a+')as f:
+            data = f.read()
 
 
 
 if __name__ == '__main__':
+    print LOG_PATH
+    # for i in UploadFile(UploadFile.file_list(LOG_PATH)):
+    #
+    #     i.upload()
+
     # tasks = [gevent.spawn(push_info(i)) for i in all_user(PID)]
     # gevent.joinall(tasks)
     # with ThreadPoolExecutor(20) as T:
     #     T.map(push_info,all_user(PID))
     # deleteinfo(1196,'192.168.1.105')
     # deleteinfo(1196,'192.168.1.115')
-    #Applylog.writelog('aaa')
-    pass
+
+    Applylog.writelog('a')
+
