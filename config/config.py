3a8787de7e4b8a5a489ecdfd8a6934333d07619b
Myjr <Myjr@123.com>
2019-07-10 17:54:33 +0800

    6

--- config/config.py
+++ config/config.py
@@ -6,15 +6,15 @@ import os
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-PID = 'MjQ0'
+PID = ''
 APPLICATIONLOG_PATH = None
 
-IP_LIST = ['192.168.1.1051','192.168.1.1151']
+IP_LIST = ['192.168.1.105','192.168.1.115']
 
 
 
 
 LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),'Log/').replace('\\','/')
-a = "4"
+
 if __name__ == '__main__':
     print LOG_PATH
